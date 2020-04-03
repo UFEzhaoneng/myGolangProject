bcc7a90961de68ad8aa8a478809af932daa8ef63
赵能 <zhaoneng@kingsoft.com>
2020-04-03 19:05:24 +0800

    init commit

---
+++ myHttpClient.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	res, err := http.Get("http://127.0.0.1:8088/hello")
+	if err != nil {
+		log.Fatal(err)
+	}
+	robots, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", robots)
+}
