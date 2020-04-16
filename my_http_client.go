e514e3e1002768af847989e499d6d9a72a9e284c
赵能 <zhaoneng@kingsoft.com>
2020-04-17 03:57:44 +0800

    添加了学生信息管理功能： 创建学生信息 查询学生信息 修改学生专业 删除学生

--- my_http_client.go
+++ my_http_client.go
@@ -5,10 +5,11 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 func main() {
-	res, err := http.Get("http://127.0.0.1:8088/hello")
+	res, err := http.PostForm("http://127.0.0.1:8088/register", url.Values{"name": {"1"}, "age": {"1"}, "profession": {"软件工程"}})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -17,5 +18,38 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s", robots)
+	fmt.Printf("%s\n", robots)
+
+	res2, err := http.PostForm("http://127.0.0.1:8088/query", url.Values{"id": {"1"}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	robots2, err := ioutil.ReadAll(res2.Body)
+	res2.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", robots2)
+
+	res3, err := http.PostForm("http://127.0.0.1:8088/alterProfession", url.Values{"id": {"1"}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	robots3, err := ioutil.ReadAll(res3.Body)
+	res3.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", robots3)
+
+	res4, err := http.PostForm("http://127.0.0.1:8088/delete", url.Values{"id": {"1"}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	robots4, err := ioutil.ReadAll(res4.Body)
+	res4.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", robots4)
 }
